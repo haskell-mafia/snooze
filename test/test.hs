f098144e349f81a826d2221860e5b7adbfaad8e7
mth <mark@hibberd.id.au>
2015-05-09 16:59:29 +1000

    Switch to disorder.

--- test/test.hs
+++ test/test.hs
@@ -1,13 +1,12 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-module Main where
 
-import           Orphanarium.Core.Main
+import           Disorder.Core.Main
 
 import qualified Test.Snooze.Url
 
 import           System.IO (IO)
 
 main :: IO ()
-main = orphanariumMain
-    [ Test.Snooze.Url.tests
+main = disorderMain [
+      Test.Snooze.Url.tests
     ]
