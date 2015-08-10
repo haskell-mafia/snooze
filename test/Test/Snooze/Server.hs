c68944a93eb5035d0d61110ab502900075df1e0b
Sharif Olorin <sio@tesser.org>
2015-08-10 04:42:31 +0000

    [wip] Only bind to free ports in tests to avoid spurious failures

--- test/Test/Snooze/Server.hs
+++ test/Test/Snooze/Server.hs
@@ -16,22 +16,26 @@ import           P
 import           Snooze.Url
 
 import           System.IO
-import           System.Random (randomRIO)
+import           System.Posix.Process
 
 import           Web.Scotty
 
 
 withServer :: ScottyM () -> (Text -> IO a) -> IO a
 withServer app f = do
-  -- FIX Find an open port rather than failing randomly
-  port <- randomRIO (10100, 65534)
-  let url' = "http://localhost:" <> (T.pack $ show port) <> "/"
   -- Check that we can connect first to avoid flakey "connection refused"
-  let connect' = bracket (connectTo "localhost" $ PortNumber (fromInteger $ toInteger port)) hClose pure
   bracket
-    (forkIO . scotty port $ app)
+    (forkIO . scotty 0 $ app)
     killThread
-    (const $ (recoverAll (limitRetries 5) connect') >> f url')
+    (const $ (recoverAll (limitRetries 5) connect') >>= (\p -> f (url' p)))
+ where
+  connect' = do
+    port <- getProcessID >>= findPort
+    bracket (connectTo "localhost" port) hClose pure
+
+  findPort _pid = hPutStrLn stderr "Implement me!" >> pure (PortNumber 80)
+
+  url' port = T.concat ["http://localhost:", T.pack (show port), "/"]
 
 withServer' :: Path -> ScottyM () -> (Url -> IO a) -> IO a
 withServer' p s a =
