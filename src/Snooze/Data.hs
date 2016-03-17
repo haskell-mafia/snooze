7fc62dbeb0e0704c7309ba11e90543ab8cbfdc9e
mth <mark@hibberd.id.au>
2016-03-17 11:53:39 +1100

    Really basic data-types for snooze improvements.

---
+++ src/Snooze/Data.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+module Snooze.Data (
+    Component (..)
+  , Query (..)
+  , Service (..)
+  , Registry (..)
+  , Entry (..)
+  , ContentType (..)
+  ) where
+
+
+import           Control.Retry (RetryPolicy)
+
+import           Data.ByteString (ByteString)
+import           Data.String (IsString)
+import           Data.Text (Text)
+import           Data.Map.Strict (Map)
+
+import           P
+
+import           Snooze.Balance.Data (UpdatableBalanceTable)
+
+newtype Component =
+  Component {
+      component :: Text
+    } deriving (Eq, Show, Ord, IsString)
+
+data Query =
+  Query {
+      queryKey :: Text
+    , queryValue :: Text
+    } deriving (Eq, Show)
+
+newtype Service =
+  Service {
+      serviceName :: Text
+    } deriving (Eq, Show, Ord)
+
+newtype ContentType =
+  ContentType {
+      contentType :: ByteString
+    } deriving (Eq, Show, Ord)
+
+data Entry =
+  Entry {
+      entryContentType :: ContentType
+    , entryBalanceTable :: UpdatableBalanceTable
+    , entryRetryPolicy :: RetryPolicy
+    }
+
+newtype Registry =
+  Registry {
+      mappings :: Map Service Entry
+    }
